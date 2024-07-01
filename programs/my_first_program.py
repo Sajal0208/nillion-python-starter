76fe42d439d06dc45edb34b2866638120f4a9ad1
Sajal0208 <sajaldewangan@gmail.com>
2024-07-01 09:08:52 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -4,8 +4,11 @@ def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    # compute the sum of my_int1 and my_int2
-    result = my_int1 + my_int2
-
-    return [Output(result, "sum_output", party1)]
+    my_int3 = SecretInteger(Input(name="my_int3", party=party1))
+    
+    # Perform some creative computation
+    sum_ints = my_int1 + my_int2
+    product_ints = my_int1 * my_int2
+    complex_computation = sum_ints * my_int3 + product_ints - my_int2
+    
+    return [Output(complex_computation, "my_output", party1)]
